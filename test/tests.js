32f5308ebdf14dd957548269fe804f427db6020f
ricardozv <ricardosilvanet03@gmail.com>
2020-02-18 11:56:58 -0500

    up tests.js

---
+++ test/tests.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+describe('Star Wars Tests', function(){
+    it('deve burscar o r2d2 com o formato correto', async () => {
+        const expected = [{
+            nome: 'R2-D2',
+            peso: '96'
+        }]
+        const nomeBase = `r2-d2`
+    })
+})
